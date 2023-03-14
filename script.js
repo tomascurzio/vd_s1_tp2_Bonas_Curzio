9a26980904ceddf90614118d5e4740e485f2d7ba
tomascurzio <tomascurzio@gmail.com>
2023-03-14 11:28:51 -0300

    agg

---
+++ script.js
@@ -0,0 +1,14 @@
+d3.csv('astronautas.csv', d3.autoType).then(data => {
+    console.log(data)
+    // Guardamos el svg generado en la variable chart
+    let chart = Plot.plot({
+      marks: [
+        Plot.dot(data, {
+          x: 'anio_mision',
+        }),
+      ],
+    })
+    // Agregamos chart al div#chart de index.html
+    d3.select('#chart').append(() => chart)
+  })
+  
---
+++ vis_1/script.js
@@ -0,0 +1,25 @@
+d3.csv('astronautas.csv', d3.autoType).then(data => {
+    console.log(data)
+    let chart = Plot.plot({
+      marks: [
+        Plot.dot(data, {
+          x: 'anio_mision',
+        }),
+      ],
+    })
+    d3.select('#vis').append(() => chart)
+  })
+
+// d3.csv('astronautas.csv', d3.autoType).then(data => {
+//     console.log(data)
+//     let chart = Plot.plot({
+//       marks: [
+//         Plot.dot(data, {
+//           x: 'anio_mision',
+//           y: 'nacionalidad',
+//         }),
+//       ],
+//     })
+//     d3.select('#vis').append(() => chart)
+//   })
+  
